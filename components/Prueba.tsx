05b8c57c854a4a9e910f7c0afcf5304a5bd5e179
TFG3A <alfonsoeljedi@gmail.com>
2024-11-17 18:20:55 +0100

    Primer Update

---
+++ components/Prueba.tsx
@@ -0,0 +1,8 @@
+import { ThemedView } from '@/components/ThemedView';
+import { View } from 'react-native-reanimated/lib/typescript/Animated';
+
+export function Prueba(){
+    return(
+        <View></View>
+    )
+}
