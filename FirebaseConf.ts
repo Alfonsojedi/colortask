29ad02e5ccbe97d4310f0f2f1f55cc8935377419
TFG3A <alfonsoeljedi@gmail.com>
2024-11-23 17:18:02 +0100

    NavigationContainer fix

--- FirebaseConf.ts
+++ FirebaseConf.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,4 +20,3 @@ const firebaseConfig = {
 // Initialize Firebase
 export const fire_app = initializeApp(firebaseConfig);
 export const fire_auth = getAuth(fire_app);
-const analytics = getAnalytics(fire_app);
--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,13 +1,33 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
+import React, { useState} from 'react';
+import { fire_auth } from '@/FirebaseConf';
 
-
-export default function Login() {
+const Login = () => {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [loading,setLoading] = useState(false);
+  const auth = fire_auth;
   return(
-  <div>
-    <h1>Login</h1>
-    <Image
-      source={require('@/assets/images/JF.png')}
-    />
-  </div>
+  <View style={styles.container}>
+    <TextInput value={email} style={styles.input} onChangeText={(text) => setEmail(text)}></TextInput>
+  </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    flex: 1,
+    justifyContent: 'center'
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: '#fff'
+  }
+});
+
+export default Login;
