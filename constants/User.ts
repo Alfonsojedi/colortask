e8d516115686a5ad9bb0f58c888c174167a6fa10
TFG3A <alfonsoeljedi@gmail.com>
2024-12-15 10:41:38 +0100

    UPDATE User

--- constants/User.ts
+++ constants/User.ts
@@ -1,6 +1,5 @@
 import { fire_auth } from "@/FirebaseConf";
 
  export default function User(){
-    return fire_auth.currentUser?.email?.slice(0,fire_auth.currentUser?.email?.lastIndexOf('.'));
-    
+    return fire_auth.currentUser?.email?.slice(0,fire_auth.currentUser?.email?.lastIndexOf('.'));  
  }
